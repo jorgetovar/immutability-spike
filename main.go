995f9f4a24f9ad525089d6a637b5608c658da788
jorgetovar <jorgetovar621@gmail.com>
2022-09-20 22:01:56 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
+)
+
+type Book struct {
+	Author string
+	Title  string
+	Pages  int
+}
+
+func (book Book) GetPagesWithCode() string {
+	var n = rand.Intn(200)
+	pages := book.Pages - n
+	return "The " + strings.ToUpper(book.Title) + " book has " + strconv.Itoa(pages) + " pages"
+}
+
+func main() {
+
+	var book = Book{Author: "Rich Hickey", Title: "Clojure for the Brave & True", Pages: 232}
+	fmt.Printf("1) %+v\n", book)
+	changeAuthor(&book, "Daniel Higginbotham")
+	fmt.Printf("2) %+v\n", book)
+	fmt.Println(book.GetPagesWithCode())
+	fmt.Printf("3) %+v\n", book)
+
+}
+
+func changeAuthor(book *Book, author string) {
+	book.Author = author
+}
